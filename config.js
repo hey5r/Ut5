8322f8490e2bb485d5d7fbbb24de695cd0d8fb40
rikkqa43 <ranusuta8@gmail.com>
2025-06-03 02:12:49 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+if (fs.existsSync("config.env"))
+  require("dotenv").config({ path: "./config.env" });
+
+function convertToBool(text, fault = "true") {
+  return text === fault ? true : false;
+}
+module.exports = {
+  SESSION_ID: process.env.SESSION_ID || "dU5QAJSb#80X2hGoyenhvzpwfSBaOM0vM5_PgeLfBkHTcaeOaaek",
+  OWNER_NUM: process.env.OWNER_NUM || "94783462955",
+  PREFIX: process.env.PREFIX || ".",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/nMXpJFxD/jpg.jpg",
+  ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am 𝚁𝙰𝙽𝚄-𝙼𝙳. I am alive now!!\n\n🥶𝐌𝐚𝐝𝐞 𝐛𝐲 𝚁_𝙰_𝙽_𝙳_𝚄_𝙻_𝙰🥶",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+  MODE: process.env.MODE || "public",
+};
+
